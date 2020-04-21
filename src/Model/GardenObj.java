221f4f95bcaa0cf421a222f0fc033d52ec85faeb
Ian-McCabe <imccabe@udel.edu>
2020-04-21 13:24:06 -0400

    Added gardenobj

--- src/Model/GardenObj.java
+++ src/Model/GardenObj.java
@@ -1,5 +1,15 @@
 package Model;
 
 public class GardenObj {
-
+	private int ID;
+	private int xLoc;
+	private int yLoc; 
+	private int area;
+	private String name; 
+	//Should there be an image associated with each object?
+	
+	
+	public void drawImage() {
+		
+	}
 }
