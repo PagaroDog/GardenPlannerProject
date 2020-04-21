01145463752907dd3952fbc35f47df89c2feadf2
Brandon Wu <brandonjameswu1@gmail.com>
2020-04-21 13:36:49 -0400

    Added test for nextButton and comboboxes to Pref.View

--- src/Tests/PreferencesControllerTests.java
+++ src/Tests/PreferencesControllerTests.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 
 import Controllers.PreferencesController;
+import Model.GardenPref;
 import Model.Model;
 import Model.StageName;
 import Views.PreferencesView;
@@ -21,11 +22,13 @@ public class PreferencesControllerTests {
 		PreferencesController pc = new PreferencesController();
 		PreferencesView pv = new PreferencesView(new Stage());
 		MouseEvent me = new MouseEvent(null, 0, 0, 0, 0, 0, 0, false);
+		//Mod.createGardenPref("Name",pv.getSun(),pv.getBloom(),pv.getSoil(),pv.getColor(),null,null,null);
 		
 		Mod.setStageName(StageName.PREFERENCES);
 		pc.gethandleOnNextButton();
 		assertEquals(StageName.DESIGN, Mod.getStageName());
-		
+		GardenPref test = new GardenPref("Name",pv.getSun().getValue(),pv.getBloom().getValue(),pv.getSoil().getValue(),pv.getColor().getValue(),null,null,null));
+		assertEquals(test,Mod.getGardenPref());
 		
 	}
 	@Test
@@ -34,6 +37,7 @@ public class PreferencesControllerTests {
 		PreferencesController pc = new PreferencesController();
 		PreferencesView pv = new PreferencesView(new Stage());
 		MouseEvent me = new MouseEvent(null, 0, 0, 0, 0, 0, 0, false);
+		
 		
 		Mod.setStageName(StageName.PREFERENCES);
 		pc.gethandleOnBackButton();
--- src/Views/PreferencesView.java
+++ src/Views/PreferencesView.java
@@ -1,11 +1,19 @@
 package Views;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 public class PreferencesView extends View{
 	private Button nextButton;
 	private Button backButton;
+	private ComboBox<String> soil;
+	private ComboBox<String> water;
+	private ComboBox<String> sun;
+	private ComboBox<String> color;
+	private ComboBox<String> bloom;
+	
+	
 	public PreferencesView(Stage stage) {
 		// TODO Auto-generated constructor stub
 	}
@@ -18,5 +26,28 @@ public class PreferencesView extends View{
 
 	public Button getNextButton() {
 		return nextButton;
+	}
+	public Button getBackButton() {
+		return backButton;
+	}
+
+	public ComboBox<String> getSoil() {
+		return soil;
+	}
+
+	public ComboBox<String> getWater() {
+		return water;
+	}
+
+	public ComboBox<String> getSun() {
+		return sun;
+	}
+
+	public ComboBox<String> getColor() {
+		return color;
+	}
+
+	public ComboBox<String> getBloom() {
+		return bloom;
 	}
 }
