ca50a324eb02888cde5f2238c875ae31d845f553
Ian-McCabe <imccabe@udel.edu>
2020-04-21 13:17:52 -0400

    Added two methods to conitions controller.java

--- src/Controllers/ConditionsController.java
+++ src/Controllers/ConditionsController.java
@@ -1,5 +1,15 @@
 package Controllers;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
 
 public class ConditionsController {
-
+	
+	public EventHandler handleOnSelectButtom(MouseEvent event) {
+		return null; 
+	}
+	
+	public EventHandler handleOnDeleteButton(MouseEvent event){
+		return null; 
+	}
 }
