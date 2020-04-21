f8a8f605663193d983e58578189da7834c484aa6
Brandon Wu <brandonjameswu1@gmail.com>
2020-04-20 21:28:04 -0400

    Added JavaDoc information

--- src/Controllers/PreferencesController.java
+++ src/Controllers/PreferencesController.java
@@ -7,20 +7,39 @@ public class PreferencesController {
 
 	
 	
-	
+/**
+ * Returns an EventHandler for when the Back Button is pressed on the Preferences scene.
+ * 
+ * @param event MouseEvent
+ * @return EventHandler
+ */
 	
 	public EventHandler handleOnBackButton(MouseEvent event) {
 		return null;
 	}
+/**
+ * This will send the user back to the ConditionsView stage where they can continue to label
+ * the conditions of ranges in the garden. Sets the StageName to StageName.CONDITIONS.
+ * @param event
+ */
 	public void BackButton(MouseEvent event) {
 		
 	}
 	
-	
+/**
+ * Returns an EventHandler for when the next button is pressed on the Preferences scene.
+ * @param event
+ * @return EventHandler
+ */
 	public EventHandler handleOnNextButton(MouseEvent event) {
 		return null;
 	}
-	
+/**
+ * This will send the user to the GardenView stage. Sets the StageName to StageName.Design. 
+ * Saves the users preferences in the Model's userPreferences collection. Creates the collection
+ * of suggested plants from preferences selected. 
+ * @param event
+ */
 	public void NextButton(MouseEvent event) {
 		
 	}
