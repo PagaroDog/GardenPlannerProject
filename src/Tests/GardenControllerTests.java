792a32e00fd159c6c6ac0d05ce9657f5e03708b1
mattc2x4@yahoo.com <mattc2x4@yahoo.com>
2020-04-21 16:17:38 -0400

    tests written, many todo

--- src/Tests/GardenControllerTests.java
+++ src/Tests/GardenControllerTests.java
@@ -4,11 +4,57 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Controllers.GardenController;
+import Controllers.StartupController;
+import Model.Model;
+import Views.GardenView;
+import Views.StartupView;
+import javafx.stage.Stage;
+
 public class GardenControllerTests {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Model model = new Model();
+		GardenView gv = new GardenView(new Stage());
+		GardenController gc = new GardenController();
+		
+		gc.handleOnFallButton();
+		assertEquals(model.getSEASON(), SEASON.FALL);
+		
+		gc.handleOnWinterButton();
+		assertEquals(model.getSEASON(), SEASON.WINTER);
+		
+		gc.handleOnSpringButton();
+		assertEquals(model.getSEASON(), SEASON.SPRING);
+		
+		gc.handleOnSummerButton();
+		assertEquals(model.getSEASON(), SEASON.SUMMER);
+		
+		//TODO: figure out what this should do
+		gc.handleOnStatsButton();
+		assertEquals(model.getSEASON(), SEASON.FALL);
+		
+		//TODO: figure out what this should effect.
+		gc.handleOnYear1Button();
+		assertEquals(model.getSEASON(), SEASON.FALL);
+		
+		//TODO: figure out what this should effect.
+		gc.handleOnYear2Button();
+		assertEquals(model.getSEASON(), SEASON.FALL);
+				
+		//TODO: figure out what this should effect.
+		gc.handleOnYear3Button();
+		assertEquals(model.getSEASON(), SEASON.FALL);
+		
+		
+		//TODO: figure out what this should effect.
+		gc.handleOnImgDrag();
+		assertEquals(model.getSEASON(), SEASON.FALL);
+		
+		//TODO: figure out what this should effect.
+		gc.handleOnPrefButton();
+		assertEquals(model.getSEASON(), SEASON.FALL);
 	}
 
 }
--- src/Tests/StartupControllerTests.java
+++ src/Tests/StartupControllerTests.java
@@ -1,14 +1,22 @@
 package Tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import Controllers.StartupController;
+import Model.Model;
+import Views.StartupView;
+import javafx.stage.Stage;
 
 public class StartupControllerTests {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Model model = new Model();
+		StartupView suv = new StartupView(new Stage());
+		StartupController suc = new StartupController();
+		suc.handleOnNewButton();
+		assertEquals(model.stageName,
 	}
 
 }
