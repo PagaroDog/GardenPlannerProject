bfd4ba671f19afe68cdbf261e747c6f9d188b215
Ian-McCabe <imccabe@udel.edu>
2020-04-21 12:45:18 -0400

    modified model.java, testing git

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,7 +1,12 @@
 package Model;
 
+import java.util.Collection;
+
 public class Model {
-	
+    private Season season = new Season(); 
+    private StageName stageName; 
+    Collection<GardenPref> gardenpreferences; 
+    
 
 	
 	
