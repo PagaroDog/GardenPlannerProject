ee487086ec9229286be7599d9f51374e77c4c0f9
Ian-McCabe <imccabe@udel.edu>
2020-04-21 13:32:16 -0400

    Added all plant.java attributes and getters/setters

--- src/Model/Plant.java
+++ src/Model/Plant.java
@@ -1,5 +1,63 @@
 package Model;
 
 public class Plant {
-
+	private String name; 
+	private int light; 
+	private int bloomtime; 
+	private String soilType; 
+	private double waterlevel; 
+	private String color; 
+	private int animalsFed; 
+	private int pollinator; //is this whether or not the plant pollinates or
+	//how many things it pollinates 
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLight() {
+		return light;
+	}
+	public void setLight(int light) {
+		this.light = light;
+	}
+	public int getBloomtime() {
+		return bloomtime;
+	}
+	public void setBloomtime(int bloomtime) {
+		this.bloomtime = bloomtime;
+	}
+	public String getSoilType() {
+		return soilType;
+	}
+	public void setSoilType(String soilType) {
+		this.soilType = soilType;
+	}
+	public double getWaterlevel() {
+		return waterlevel;
+	}
+	public void setWaterlevel(double waterlevel) {
+		this.waterlevel = waterlevel;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getAnimalsFed() {
+		return animalsFed;
+	}
+	public void setAnimalsFed(int animalsFed) {
+		this.animalsFed = animalsFed;
+	}
+	public int getPollinator() {
+		return pollinator;
+	}
+	public void setPollinator(int pollinator) {
+		this.pollinator = pollinator;
+	}
+	
 }
