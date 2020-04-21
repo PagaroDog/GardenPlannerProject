2fd804fae1be548d33275bb5e3704a4f0e0f1f4c
Ian-McCabe <imccabe@udel.edu>
2020-04-21 13:00:59 -0400

    added method stubs to model.java

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -5,9 +5,67 @@ import java.util.Collection;
 public class Model {
     private Season season = new Season(); 
     private StageName stageName; 
-    Collection<GardenPref> gardenpreferences; 
+    private Collection<GardenPref> gardenpreferences; 
+    private Collection<GardenObj> gardenObjects; 
+    private Collection<Suggestions> suggestions; 
+    private Collection<Actions> actions; 
+ 
     
-
+    private int canvasWidth; 
+    private int canvasHeight; 
+    
+    
+    public void addGardenObj() {
+    	
+    }
+    
+    public void addGardenpref() {
+    	
+    }
+    
+    public void removeGardenObj() {
+    	
+    }
+    
+    public void removeGardenPref() {
+    	
+    }
+    
+    public void modifyGardenobj() {
+    	
+    }
+    
+    public void modifyGardenPref() {
+    	
+    }
+    
+    public void createSuggestion() {
+	   
+    }
+    
+    public void saveGarden() {
+    	
+    }
+    
+    public void loadGarden() {
+    	
+    }
+    
+    public void mode() {
+    	
+    }
+    
+    public void copy() {
+    	
+    }
+    
+    public void undo() {
+    	
+    }
+    
+    public void redo() {
+    	
+    }
 	
 	
 }
