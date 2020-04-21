58863e8987732474275ef779560dbbd5f8d60db3
Tommy White <tommywhite525@gmail.com>
2020-04-20 20:34:05 -0400

    DrawYard done

--- src/Controllers/DrawYardController.java
+++ src/Controllers/DrawYardController.java
@@ -1,5 +1,39 @@
 package Controllers;
 
-public class DrawYardController {
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
+public class DrawYardController {
+	
+	/**
+	 * Handles event when user presses rectangle button
+	 * @return EventHandler object for this action
+	 */
+	public EventHandler handleOnRectButton() {
+		return null;
+	}
+	
+	/**
+	 * Handles event when user presses circle button
+	 * @return EventHandler object for this action
+	 */
+	public EventHandler handleOnCircleButton() {
+		return null;
+	}
+	
+	/**
+	 * Handles event when user presses import button
+	 * @return EventHandler object for this action
+	 */
+	public EventHandler handleOnImportButton() {
+		return null;
+	}
+	
+	/**
+	 * Handles event when user drags across the main pain
+	 * @return EventHandler object for this action
+	 */
+	public EventHandler handleOnDragPane() {
+		return null;
+	}
 }
--- src/Views/DrawYardView.java
+++ src/Views/DrawYardView.java
@@ -1,15 +1,26 @@
 package Views;
+import java.awt.Button;
+
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class DrawYardView extends View{
 	private Stage stage;
+	private Button rectButton;
+	private Button circleButton;
+	private Button importButton;
+	private Pane drawing;
+	
 	public DrawYardView(Stage stage) {
 		this.stage = stage;
 	}
+	/**
+	 * Gets the Draw Yard screen's Scene object
+	 * @return Scene object for the Draw Yard screen
+	 */
 	@Override
 	public Scene getScene() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
