23025e1f7432ccddf1a36622dd52b93aff302cc7
Ian-McCabe <imccabe@udel.edu>
2020-04-21 21:37:49 -0400

    Added model tests, class, and garden object classes

--- src/Model/GardenObj.java
+++ src/Model/GardenObj.java
@@ -8,8 +8,74 @@ public class GardenObj {
 	private String name; 
 	//Should there be an image associated with each object?
 	
+	public GardenObj() {
+		
+	}
 	
+	
+	
+	public int getID() {
+		return ID;
+	}
+
+
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+
+
+	public int getxLoc() {
+		return xLoc;
+	}
+
+
+
+	public void setxLoc(int xLoc) {
+		this.xLoc = xLoc;
+	}
+
+
+
+	public int getyLoc() {
+		return yLoc;
+	}
+
+
+
+	public void setyLoc(int yLoc) {
+		this.yLoc = yLoc;
+	}
+
+
+
+	public int getArea() {
+		return area;
+	}
+
+
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
 	public void drawImage() {
 		
 	}
+	
 }
--- src/Tests/ModelTests.java
+++ src/Tests/ModelTests.java
@@ -4,11 +4,74 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Model.GardenObj;
+import Model.GardenPref;
+import Model.Model;
+
 public class ModelTests {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void copyTest() {
+		Model test = new Model(); 
+		GardenObj j = new GardenObj(); 
+		
+		test.getGardenObjects().put(j, 1); 
+		
+		test.copy(j, j.getID()); 
+		
+		assertEquals(test.getGardenObjects().size(), 2); 
+	}
+	
+	
+	@Test
+	public void undoTest() {
+		Model test = new Model(); 
+		GardenObj j = new GardenObj(); 
+		
+		test.getGardenObjects().put(j, 1); 
+		
+		test.undo();
+		
+		assertEquals(test.getGardenObjects().size(), 0); 
+	}
+	
+	public void redoTest() {
+		Model test = new Model(); 
+		GardenObj j = new GardenObj(); 
+		
+		test.getGardenObjects().put(j, 1); 
+		
+		test.undo();
+		test.redo(); 
+		
+		assertEquals(test.getGardenObjects().size(), 1);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
