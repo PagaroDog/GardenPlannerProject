67c74d652e12c80d8e7c3f0d53af75e9065e8d99
Brandon Wu <brandonjameswu1@gmail.com>
2020-04-24 16:42:16 -0400

    Create empty Suggestions View and Controller combination

---
+++ src/Controllers/SuggestionsController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import Model.Model;
+import Views.SuggestionsView;
+
+public class SuggestionsController extends Controller<SuggestionsView>{
+
+	public SuggestionsController(Model model, SuggestionsView view) {
+		super(model, view);
+	}
+
+}
---
+++ src/Views/SuggestionsView.java
@@ -0,0 +1,33 @@
+package Views;
+
+import Controllers.Controller;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SuggestionsView extends View{
+
+	@Override
+	public Scene getScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setController(Controller controller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Stage getStage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
