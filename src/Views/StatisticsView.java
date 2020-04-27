d2669aaf22f18dc44d604b17680dd9fe8744d562
Ian-McCabe <imccabe@udel.edu>
2020-04-26 20:51:48 -0400

    Added statistics labels in StatisticsView.java

--- src/Views/StatisticsView.java
+++ src/Views/StatisticsView.java
@@ -3,9 +3,13 @@ import javafx.scene.control.Button;
 
 import Controllers.Controller;
 import Controllers.StatisticsController;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -22,12 +26,49 @@ public class StatisticsView extends View{
 	}
 	
 	public void setup() {
+		
+		
 		TilePane tp = new TilePane();
 		Label txt = new Label("Stats");
 		backButton = new Button("Back");
 		backButton.setOnMouseClicked(control.handleOnBackButton());
 		tp.getChildren().addAll(txt,backButton);
-		scene = new Scene(tp, canvasWidth, canvasHeight);
+		//scene = new Scene(tp, canvasWidth, canvasHeight);
+		
+		BorderPane border = new BorderPane(); 
+		border.setTop(tp); 
+		
+		VBox vBox = new VBox(); 
+		
+		Label pollinators = new Label(); 
+		pollinators.setText("Total Plants: , Trees: , Flowers: , Shrubs: ");
+		pollinators.setPadding(new Insets(10,10,10,10));
+		pollinators.setStyle("-fx-background-color: rgba(158,255,174,1);");
+		
+		Label totalPlants = new Label("Total Keystone Plants:"); 
+		totalPlants.setPadding(new Insets(10,10,10,10));
+		totalPlants.setStyle("-fx-background-color: rgba(158,255,174,1);"); 
+		
+		Label totalTrees = new Label("Estimated Animals Fed:"); 
+		totalTrees.setPadding(new Insets(10,10,10,10));
+		totalTrees.setStyle("-fx-background-color: rgba(158,255,174,1);");
+		
+		
+		Label waterSaved = new Label("Types of Animals Fed:"); 
+		waterSaved.setPadding(new Insets(10,10,10,10));
+		waterSaved.setStyle("-fx-background-color: rgba(158,255,174,1);");
+		
+		
+		
+		
+		vBox.getChildren().addAll(pollinators, totalPlants, totalTrees, waterSaved); 
+		
+		
+		border.setLeft(vBox);
+		
+		scene = new Scene(border, canvasWidth, canvasHeight); 
+		
+		
 	}
 
 	/**
