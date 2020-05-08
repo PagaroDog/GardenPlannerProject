ee9686f31421c9288539f2fa71f5e7b2eff6400e
BJW033 <Birdfan002@gmail.com>
2020-05-08 18:36:25 -0400

    ModelTest: Cleared and set failure for copy, undo, and redo test.
    Added test for getUserPicks

--- src/Tests/ModelTests.java
+++ src/Tests/ModelTests.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import Model.GardenObj;
 import Model.GardenPref;
 import Model.Model;
+import Model.Plant;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 /**
  * 
@@ -15,46 +17,43 @@ import Model.Model;
  */
 public class ModelTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+
 
 	@Test
 	public void copyTest() {
-		Model test = new Model();
-		GardenObj j = new GardenObj(0, 0, 0, 0, null);
-
-		test.getGardenObjects().put(1, j);
-
-		test.copy(j, j.getID());
-
-		assertEquals(test.getGardenObjects().size(), 2);
+		fail("method not made");
 	}
 
 	@Test
 	public void undoTest() {
-		Model test = new Model();
-		GardenObj j = new GardenObj(0, 0, 0, 0, null);
-
-		test.getGardenObjects().put(1, j);
-
-		test.undo();
-
-		assertEquals(test.getGardenObjects().size(), 0);
+		fail("method not made");
 	}
-
+	@Test
 	public void redoTest() {
-		Model test = new Model();
-		GardenObj j = new GardenObj(0, 0, 0, 0, null);
-
-		test.getGardenObjects().put(1, j);
-
-		test.undo();
-		test.redo();
-
-		assertEquals(test.getGardenObjects().size(), 1);
-
+		fail("method not made");
+	}
+	@Test
+	public void getUserPicksTest() {
+		Model test  = new Model();
+		int rows = 3;
+		int cols = 7;
+		int pick = (int)Math.random()*(rows*cols);
+		Plant plant = test.getSuggestedPlants().get(pick);
+		int index = 0;
+		GridPane grid = new GridPane();
+		String style = "-fx-background-color: BLACK;";
+		for(int i = 0;i<rows;i++) {
+			for(int j= 0;j<cols;j++) {
+				if(pick == index) {
+					ImageView iv = new ImageView();
+					iv.setStyle(style);
+					grid.getChildren().add(new ImageView());
+				}
+				else
+					grid.getChildren().add(new ImageView());
+			}
+		}
+		assertEquals(plant, test.getSuggestedPlants().get(0));
 	}
 
 }
