f35648f71eb26f21aafa8e43c611fc1fc316453f
Ian-McCabe <imccabe@udel.edu>
2020-05-17 13:03:39 -0400

    Added javadoc for my methods in GardenView

--- src/Views/GardenView.java
+++ src/Views/GardenView.java
@@ -49,7 +49,7 @@ import javafx.stage.Stage;
  * graphical logic.
  * 
  * @author Matt Cohen
- *
+ * @author IanMcCabe
  */
 public class GardenView extends View<GardenController> implements Serializable {
 	private HashMap<String, Image> plantImages = new HashMap<String, Image>();
@@ -475,7 +475,17 @@ public class GardenView extends View<GardenController> implements Serializable {
 
 		this.garden.getChildren().add(vb);
 	}
-
+	
+	
+	/**
+	 * This method displays the info of the current plant that the mouse is over 
+	 * 
+	 * 
+	 * @param e The circle the user is currently over 
+	 * @param mouseX The X coordinate of the mouse 
+	 * @param mouseY The Y coordinate of the mouse 
+	 * @param plantMatch The name of the plant being displayed
+	 */
 	public void displayInfo(Circle e, double mouseX, double mouseY, String plantMatch) {
 		ImageWithSourceInfo[] img = imgs.getPlantImages().get(e.getUserData());
 		Image i = img[0].getImg();
@@ -505,7 +515,10 @@ public class GardenView extends View<GardenController> implements Serializable {
 		}
 
 	}
-
+	
+	/**
+	 * Removes the info of a plant when the user's mouse has left the plant object 
+	 */
 	public void removeInfo() {
 		this.garden.getChildren().remove(info);
 	}
@@ -550,6 +563,12 @@ public class GardenView extends View<GardenController> implements Serializable {
 	}
 	
 	
+	/**
+	 * This method iterates over a list of garden actions drawing all of the users inputs to the gardenView.
+	 * The list of GardenActions is modified by the GardenAction class to remove/add actions the user has done
+	 * to undo/redo objects from the screen. 
+	 * @param GA The list of GardenActions the method goes through to draw the users inputs back on the screen. 
+	 */
 	public void iterateGardenActions(GardenAction GA) {
 		System.out.println("Iterating over garden actions"); 
 		garden.getChildren().clear(); 
